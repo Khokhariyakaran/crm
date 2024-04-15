c692965197bb7a734c26100cf8abb17c1cd7d8cf
Khokhariyakaran <khokhariyakaran911@gmail.com>
2024-04-15 18:58:55 +0530

    third commit

--- crm_web/views.py
+++ crm_web/views.py
@@ -1,7 +1,56 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import *
+from .models import *
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    records = Records.objects.all()
+
+    if request.method == 'POST':
+        # check to see if login in
+        username = request.POST['username']
+        password = request.POST['password']
+        # authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In Successfully")
+            return redirect('home')
+        else:
+            messages.success(
+                request, "There Was An Error Logging In, Please Try Again...")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {'records': records})
+
+
+# def login_user(request):
+#     pass
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You Have Been Logged Out Successfully")
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(
+                request, 'You Have Successfully Register Your Account')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
