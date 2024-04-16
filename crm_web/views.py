16b4f8191bc4846659b1d41ca6a6f2c30b3e5297
Khokhariyakaran <khokhariyakaran911@gmail.com>
2024-04-16 16:59:07 +0530

    fourth commit

--- crm_web/views.py
+++ crm_web/views.py
@@ -8,7 +8,6 @@ from .models import *
 
 def home(request):
     records = Records.objects.all()
-
     if request.method == 'POST':
         # check to see if login in
         username = request.POST['username']
@@ -54,3 +53,51 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form': form})
     return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Records.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You Must Be Logged In To View Page")
+        return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Records.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record Deleted Succesfully")
+        return redirect('home')
+    else:
+        messages.success(request, "You Must Be Logged In First")
+        return redirect('home')
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, "You Must Be Logged In First")
+        return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Records.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record Has Been Updated Succesfully")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, "You Must Be Logged In First")
+        return redirect('home')
